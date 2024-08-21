f722ef8825e59954aea10e600c5d64817bdb0fd2
LokeshwarMERN <lokeshwar.prasad.cse@gmail.com>
2024-08-21 23:56:09 +0530

    added mouse follower

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import { RouterProvider } from 'react-router-dom';
 import routes from './routes/routes.tsx';
+import FollowCursor from './components/animation/FollowCursor.tsx';
 
 const App: React.FC = () => {
   return (
     <>
       <RouterProvider router={routes} />
+      <FollowCursor />
     </>
   );
 };
