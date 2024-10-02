c119ed8bf81acb49930083ede12dd9e433e1cb04
LokeshwarMERN <lokeshwar.prasad.cse@gmail.com>
2024-10-02 19:53:50 +0530

    Feat : Add Expenses API Integrated

--- client/src/services/expenses.ts
+++ client/src/services/expenses.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosRequestConfig } from 'axios';
 
+// GET EXPENSES BY DATE
 interface ExpensesResTypes {
   statusCode: number;
   data: {
@@ -34,6 +35,41 @@ export const getExpensesByDate = async (
   };
   const { data } = await axios.post<ExpensesResTypes>(
     'http://localhost:5000/api/user/show-past-date-expenses',
+    credentials,
+    config
+  );
+  return data;
+};
+
+// ADD EXPENSES OF DATE OR TODAY
+interface ExpensesResTypes {
+  statusCode: number;
+  message: string;
+  success: boolean;
+}
+interface ExpensesCredentialsType {
+  date: string; 
+  pastDaysExpensesArray: {
+    date: string;
+    productsArray: {
+      name: string;
+      price: number;
+      category: string;
+    }[];
+  }[];
+}
+
+export const addExpenses = async (
+  credentials: ExpensesCredentialsType
+): Promise<ExpensesResTypes> => {
+  const config: AxiosRequestConfig = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    withCredentials: true,
+  };
+  const { data } = await axios.post<ExpensesResTypes>(
+    'http://localhost:5000/api/user/add-past-date-expenses',
     credentials,
     config
   );
