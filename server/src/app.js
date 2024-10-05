8eea8904ea8ae544d753aff272c71359c16e49ba
LokeshwarMERN <lokeshwar.prasad.cse@gmail.com>
2024-10-06 02:03:46 +0530

    Fixed CORS issue

--- server/src/app.js
+++ server/src/app.js
@@ -6,12 +6,18 @@ import userRoutes from "./routes/user.routes.js"
 import userReportRoutes from "./routes/report.routes.js"
 
 // const originURL = "http://localhost:5173";
-const originURL = "*";
+const originURL = 'https://mybudgetter.netlify.app';
 
 app.use(cors({
     origin: originURL,
     credentials: true,
 }))
+
+app.options('*', cors({
+    origin: originURL,
+    credentials: true,
+}));
+
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.static("public"));
