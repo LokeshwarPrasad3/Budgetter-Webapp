a67d5ce969f5a0560a63d834d4018faf0059cf9c
Lokeshwar Dewangan <lokeshwar.prasad.cse@gmail.com>
2024-11-16 18:57:51 +0530

    Added Filters functionality in reports page

--- client/src/components/user/reports/table/AllExpensesTable.tsx
+++ client/src/components/user/reports/table/AllExpensesTable.tsx
@@ -21,9 +21,14 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from '@/components/ui/popover';
-import { getPreviousMonthsName } from '@/utils/date/date';
+import {
+  getPreviousMonthsName,
+  getMonthInNumber,
+  getLast7Days,
+} from '@/utils/date/date';
 import { ListFilter } from 'lucide-react';
 import { setAllExpenses } from '@/features/expenses/expenses';
+import toast from 'react-hot-toast';
 
 interface Product {
   name: string;
@@ -103,6 +108,7 @@ const AllExpensesTable: React.FC = () => {
   // Update Redux store with fetched data when available
   useEffect(() => {
     if (allExpensesResData?.success) {
+      // console.log(allExpensesResData);
       dispatch(setAllExpenses(allExpensesResData.data)); // Ensure to access the correct data structure
     }
   }, [allExpensesResData, dispatch]); // Added dependencies
@@ -118,16 +124,57 @@ const AllExpensesTable: React.FC = () => {
     );
   };
 
+  // get filtered month
+  const handleFilterMonthExpenses = (monthName: string) => {
+    // console.log(monthName)
+    const flattenedData = flattenExpenses(allExpensesArray);
+    if (monthName === 'all') {
+      setData(flattenedData);
+      return;
+    }
+    const monthInNumber = getMonthInNumber(monthName);
+    const currentYear = new Date().getFullYear();
+    // filter data which have month name
+    const filteredMonthExpenseData = flattenedData?.filter(
+      (dayExpenses) =>
+        dayExpenses.date?.slice(3) === `${monthInNumber}-${currentYear}`
+    );
+    // console.log(filteredMonthExpenseData);
+    setData(filteredMonthExpenseData);
+  };
+
+  // Filter last 7 days expenses
+  const handleFilterExpensesTable = (open: boolean) => {
+    setIsPopoverOpen(open);
+    const getLast7DaysDates = getLast7Days();
+    // console.log(getLast7DaysDates)
+    // filter data which have month name
+    const flattenedData = flattenExpenses(allExpensesArray);
+    // console.log(flattenedData)
+    const filteredLast7DaysExpenseData = flattenedData?.filter((dayExpenses) =>
+      getLast7DaysDates.includes(dayExpenses.date)
+    );
+    // console.log(filteredLast7DaysExpenseData);
+    setData(filteredLast7DaysExpenseData);
+  };
+
+  // Show caetgories all expenses
+  const handleShowAllCategoryWiseExpenses = (open: boolean) => {
+    setIsPopoverOpen(open);
+    toast.error("Feature is pending");
+  }
+
   // If the data is available in the Redux store, flatten and set it
   useEffect(() => {
     if (allExpensesArray) {
+      // console.log(allExpensesArray); // need to filter and then set data flatten array
       const flattenedData = flattenExpenses(allExpensesArray);
       setData(flattenedData);
     }
   }, [allExpensesArray]);
 
   useEffect(() => {
-    if(!isLoading) {
+    if (!isLoading) {
       setHasLoaded(true);
     }
   }, [isLoading]);
@@ -140,9 +187,16 @@ const AllExpensesTable: React.FC = () => {
 
   return (
     <div className="allexpenses_table_container bg-[#ffffff] rounded-md w-full px-0 py-5 flex justify-center flex-col">
-      <div className="filter_expense_containerr flex justify-between items-center gap-5 flex-wrap text-base px-5 pb-4">
+      <div className="filter_expense_containerr flex justify-between items-center gap-5 flex-wrap text-base px-5 pb-4 sticky top-16 bg-white rounded-md shadow-sm">
         <p className="font-semibold">Your All Expenses</p>
         <div className="filters flex justify-center gap-1 font-medium items-center">
+          <span
+            data-tooltip-id="header-tooltip"
+            data-tooltip-content="Results Lenght"
+            className="bg-pink-600 rounded-full cursor-pointer min-w-6 max-w-full w-full min-h-5 text-xs text-white p-1 flex justify-center items-center"
+          >
+            {data?.length}
+          </span>
           <Popover onOpenChange={handlePopoverClose} open={isPopoverOpen}>
             <PopoverTrigger asChild>
               <span className="relative rounded-full hover:bg-[#f3f3f3] p-2 flex justify-center items-center">
@@ -153,27 +207,27 @@ const AllExpensesTable: React.FC = () => {
               <div className="content_container flex flex-col gap-2">
                 <p
                   className="cursor-pointer text-sm hover:scale-105"
-                  onClick={() => handlePopoverClose(false)}
+                  onClick={() => handleFilterExpensesTable(false)}
                 >
                   Last 7 Days
                 </p>
                 <p
                   className="cursor-pointer text-sm hover:scale-105"
-                  onClick={() => handlePopoverClose(false)}
+                  onClick={() => handleShowAllCategoryWiseExpenses(false)}
                 >
                   Show Category
                 </p>
               </div>
             </PopoverContent>
           </Popover>
-          <Select>
+          <Select onValueChange={handleFilterMonthExpenses}>
             <SelectTrigger className="w-full">
               <SelectValue placeholder="All Expenses" />
             </SelectTrigger>
             <SelectContent>
               <SelectItem value="all">All Expenses</SelectItem>
               <SelectGroup>
-                {getPreviousMonthsName(2).map((monthName) => (
+                {getPreviousMonthsName(5).map((monthName) => (
                   <SelectItem key={monthName} value={monthName}>
                     {monthName}
                   </SelectItem>
@@ -195,10 +249,9 @@ const AllExpensesTable: React.FC = () => {
       )}
       {isLoading && !hasLoaded ? (
         <div className="loading_effects text-semibold px-5 pb-5">
-           <div className="loader"></div>
+          <div className="loader"></div>
         </div>
-      )
-      : hasLoaded && data.length === 0 ? (
+      ) : hasLoaded && data.length === 0 ? (
         <div className="message_outer bg-[#ffffff] rounded-md w-full p-5 ">
           <div className="flex">No Expenses Found</div>
         </div>
