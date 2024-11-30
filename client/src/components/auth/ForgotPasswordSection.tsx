51910d9a63add2322863d6b9a076973b9a1d1db1
Lokeshwar Dewangan <lokeshwar.prasad.cse@gmail.com>
2024-11-30 22:16:51 +0530

    Fixed Typescript Build failed

--- client/src/components/auth/ForgotPasswordSection.tsx
+++ client/src/components/auth/ForgotPasswordSection.tsx
@@ -1,6 +1,6 @@
 // ForgotPasswordSection.tsx
 
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from '../ui/button';
 import { useMutation } from '@tanstack/react-query';
