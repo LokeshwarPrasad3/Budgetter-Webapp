8088a8f8e078b6ebabcbc8edbcb16b4d778a8510
Lokeshwar Dewangan <lokeshwar.prasad.cse@gmail.com>
2025-01-19 00:49:58 +0530

    Implemented API for add lent money, received lent money, show all lent money with routes

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser, loginUser, resetPassword, changeAvatar, validateResetPasswordToken, validateAccountVerification, addUserPocketMoney, getLoggedUserData, logoutUser, sentTokenToResetPassword, checkUserVerified, changeUserCredentials, deleteUserAccount, getAllAppUsersData } from "../controllers/user.controllers.js";
+import { registerUser, loginUser, resetPassword, changeAvatar, validateResetPasswordToken, validateAccountVerification, addUserPocketMoney, getLoggedUserData, logoutUser, sentTokenToResetPassword, checkUserVerified, changeUserCredentials, deleteUserAccount, getAllAppUsersData, AddLentMoney, getAllLentMoneyHistory, receivedLentMoney } from "../controllers/user.controllers.js";
 import { showParticularDateExpenses, addTodayExpenses, addParticularDateExpenses, showTodayExpenses, showAllDateExpenses } from "../controllers/expenses.controller.js";
 import verifyJwtToken from "../middleware/auth.middleware.js";
 import { upload } from "../middleware/multer.middleware.js"
@@ -45,5 +45,10 @@ router.route("/logout").get(verifyJwtToken, logoutUser)
 
 // get all users list
 router.route("/get-all-users").get(getAllAppUsersData);
+
+// Lent Money to some person API routes
+router.route("/add-lent-money").post(verifyJwtToken, AddLentMoney);
+router.route("/get-all-lent-money").get(verifyJwtToken, getAllLentMoneyHistory);
+router.route("/received-lent-money").post(verifyJwtToken, receivedLentMoney);
 
 export default router;
