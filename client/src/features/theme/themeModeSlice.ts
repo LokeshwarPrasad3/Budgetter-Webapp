4799bf23a687cea0b8c00d778a735119a703beb3
Lokeshwar Dewangan <lokeshwar.prasad.cse@gmail.com>
2025-02-26 22:46:22 +0530

    Implemented Logic to switch dark/light mode

---
+++ client/src/features/theme/themeModeSlice.ts
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  isDarkMode: false,
+};
+
+const themeModeSlice = createSlice({
+  name: 'themeMode',
+  initialState,
+  reducers: {
+    toggleThemeMode: (state) => {
+      state.isDarkMode = !state.isDarkMode;
+      localStorage.setItem('isDarkMode', JSON.stringify(state.isDarkMode));
+      document.body.classList.toggle('dark', state.isDarkMode);
+    },
+  },
+});
+
+export const { toggleThemeMode } = themeModeSlice.actions;
+export default themeModeSlice.reducer;
