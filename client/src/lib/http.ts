965f4630a82de59279004894ab674d7e61e06d96
Lokeshwar Dewangan <lokeshwar.prasad.cse@gmail.com>
2025-06-29 03:04:40 +0530

    Code refactor types for user auth api client

---
+++ client/src/lib/http.ts
@@ -0,0 +1,23 @@
+import { backendHostURL } from '@/services/api';
+import { getCurrentAccessToken } from '@/utils/cookie/CookiesInfo';
+import axios, { AxiosInstance } from 'axios';
+
+// 1. Create axios instance
+export const apiURL: AxiosInstance = axios.create({
+  baseURL: backendHostURL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// 2. Attach token on every request
+apiURL.interceptors.request.use((config) => {
+  const token = getCurrentAccessToken();
+  if (token) {
+    config.headers = config.headers || {};
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+
---
+++ client/src/types/auth/credentials.ts
@@ -0,0 +1,45 @@
+export interface RegisterCredentialsType {
+  username: string;
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface LoginCredentialsType {
+  username: string;
+  email: string;
+  password: string;
+}
+
+export interface SignupGoogleCredentialsType {
+  token: string;
+}
+
+export interface AddPocketMoneyCredentialType {
+  date: string;
+  amount: string;
+  source: string;
+}
+
+export interface SendResetLinkCredType {
+  email: string;
+}
+
+export interface ResetPasswordCredType {
+  userId: string;
+  newPassword: string;
+}
+
+export interface DeleteUserCredType {
+  password: string;
+}
+
+export interface UpdateUserDetailsCredType {
+  name: string;
+  dob: string;
+  currentPassword: string;
+  newPassword: string;
+  instagramLink: string;
+  facebookLink: string;
+  profession: string;
+}
